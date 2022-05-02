663cca0b03bb23c66691d29badfd73a9646e5ce0
Fernando <fbuono@fi.uba.ar>
2022-05-01 23:57:10 -0300

    desafio-bdd

---
+++ 07-desafio-bdd/options/SQLite3.js
@@ -0,0 +1,11 @@
+const options = {
+    client: 'sqlite3',
+    connection: {
+        filename: '07-desafio-bdd/db/mydb.sqlite'
+    },
+    useNullAsDefault: true
+};
+
+module.exports = {
+    options,
+};
